2f2b5a69839b2fc9ca298f078100e824a8c7f14c
DikshantBhatia <bhatiadikshant20@gmail.com>
2017-12-04 12:58:56 +0100

    test comiit

--- app.js
+++ app.js
@@ -6,17 +6,33 @@
 
     .controller('TableController',TableController);
 
-     TableController.$inject = ['$scope'];
+     TableController.$inject = ['$scope','$http'];
 
-     function TableController($scope){
+     function TableController($scope,$http){
 
-          $scope.data = {"UserMobileInfoDetails" : [{"UserId" : "U123456", "RouteManifestId" : "LEMP-84000822-10100011089",
-													"MobileDeviceId" : "MW72347477847", "Timestamp" : "20171130095555", "Status" : "ACTIVE"},
-													{"UserId" : "U123456", "RouteManifestId" : "LEMP-84000822-10100011089",
-													"MobileDeviceId" : "MW72347477847", "Timestamp" : "20171130095555", "Status" : "ACTIVE"},
-													{"UserId" : "U123456", "RouteManifestId" : "LEMP-84000822-10100011089",
-													"MobileDeviceId" : "MW72347477847", "Timestamp" : "20171130095555", "Status" : "ACTIVE"}]
-						}
+          $scope.data = null;
+		  $scope.searchedUserId = '';
+		  					
+		$scope.fetchUserDetails = function(){
+						
+						$http({
+									method: 'GET',
+									url: 'https://am-we-v13-01.azure-api.net/DB/MMS/v1.0/Tasks/internal/userDetails',
+									params: {UserId: $scope.searchedUserId},
+									headers: {
+										'Ocp-Apim-Trace' : true,
+										'Ocp-Apim-Subscription-Key' : 'a692637f62fd49638022f101ff42a72d'
+									}
+							}).then(function successCallback(response) {
+							// this callback will be called asynchronously
+								$scope.data = response; 
+							
+							}, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    
+							});
+						
+		}		
         
      };
 
